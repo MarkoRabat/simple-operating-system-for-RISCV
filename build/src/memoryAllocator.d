eb83dec566903229e48f5dc07c49bec4f89ca3a7
Marko Rabat <rm200196d@student.etf.bg.ac.rs>
2023-08-17 15:01:40 -0700

    done work on C api

--- build/src/memoryAllocator.d
+++
@@ -1,8 +0,0 @@
-build/src/memoryAllocator.o: src/memoryAllocator.cpp \
- src/../h/memoryAllocator.hpp src/../h/../lib/hw.h src/../h/print.hpp
-
-src/../h/memoryAllocator.hpp:
-
-src/../h/../lib/hw.h:
-
-src/../h/print.hpp:
--- src/syscall_c.cpp
+++ src/syscall_c.cpp
@@ -5,18 +5,20 @@
 
 void* mem_alloc(size_t size) {
     uint64 volatile x;
-
     x = (uint64) size;
     __asm__ volatile("ld a1, %0" :: "m" (x));
-    __asm__ volatile("sd a1, %0" :: "m" (x));
     x = 0x01;
     __asm__ volatile("ld a0, %0" :: "m" (x));
     __asm__ volatile ("ecall");
     return nullptr;
 }
 
-int mem_free(void*) {
-
+int mem_free(void* p) {
+    uint64 volatile x;
+    x = (uint64) p;
+    __asm__ volatile("sd a1, %0" :: "m" (x));
+    x = 0x02;
+    __asm__ volatile("ld a0, %0" :: "m" (x));
     __asm__ volatile ("ecall");
     return 0;
 }
@@ -25,27 +27,67 @@ int thread_create (
         thread_t* handle,
         void (*start_routine) (void*),
         void* arg) {
-
+    uint64 volatile x;
+    x = (uint64) handle;
+    __asm__ volatile("sd a1, %0" :: "m" (x));
+    x = (uint64) start_routine;
+    __asm__ volatile("sd a2, %0" :: "m" (x));
+    x = (uint64) arg;
+    __asm__ volatile("sd a2, %0" :: "m" (x));
+    x = 0x11;
+    __asm__ volatile("ld a0, %0" :: "m" (x));
     __asm__ volatile ("ecall");
     return 0;
+}
+
+int thread_exit() {
+    x = 0x11;
+    __asm__ volatile("ld a0, %0" :: "m" (x));
+    __asm__ volatile ("ecall");
+    return 0;
+}
+
+void thread_dispatch() {
+    x = 0x11;
+    __asm__ volatile("ld a0, %0" :: "m" (x));
+    __asm__ volatile ("ecall");
+}
+
+void thread_join (thread_t handle) {
+
 }
 
 int sem_open (
         sem_t* handle,
         unsigned init ) {
-
+    uint64 volatile x;
+    x = (uint64) handle;
+    __asm__ volatile("sd a1, %0" :: "m" (x));
+    x = (uint64) init;
+    __asm__ volatile("sd a2, %0" :: "m" (x));
+    x = 0x03;
+    __asm__ volatile("ld a0, %0" :: "m" (x));
     __asm__ volatile ("ecall");
     return 0;
 }
 
 int sem_close(sem_t handle) {
-
+    uint64 volatile x;
+    x = (uint64) handle;
+    __asm__ volatile("sd a1, %0" :: "m" (x));
+    x = 0x03;
+    __asm__ volatile("ld a0, %0" :: "m" (x));
     __asm__ volatile ("ecall");
     return 0;
 }
 
 int sem_wait(sem_t id) {
 
+    uint64 volatile x;
+    x = (uint64) handle;
+    __asm__ volatile("sd a1, %0" :: "m" (x));
+    x = 0x03;
+    __asm__ volatile("ld a0, %0" :: "m" (x));
     __asm__ volatile ("ecall");
     return 0;
 }
